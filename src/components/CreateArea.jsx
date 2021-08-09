928022753d32c136b7567d1dc959390ebcdb48af
geekcode <70449427+officialskilldev@users.noreply.github.com>
2021-08-09 10:00:16 +0530

    Update CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
